c3c9cd8cb45691807b3b6bc0e21fb3ec4d385b06
Sila <silafarisida57@gmail.com>
2022-12-15 19:27:01 +0700

    adding MessageDataSource

---
+++ app/src/main/java/com/example/mysimplecleanarchitecture/data/MessageDataSource.kt
@@ -0,0 +1,7 @@
+package com.example.mysimplecleanarchitecture.data
+
+import com.example.mysimplecleanarchitecture.domain.MessageEntity
+
+class MessageDataSource: IMessageDataSource {
+    override fun getMessageFromSource(name: String) = MessageEntity("Hello $name! Welcome to Clean Architecture")
+}
