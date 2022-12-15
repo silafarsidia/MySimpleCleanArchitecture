4bd3dca9a3a4cfaa0952f31d6899917c6af5c7f3
Sila <silafarisida57@gmail.com>
2022-12-15 20:03:58 +0700

    adding MessageRepository

---
+++ app/src/main/java/com/example/mysimplecleanarchitecture/data/MessageRepository.kt
@@ -0,0 +1,8 @@
+package com.example.mysimplecleanarchitecture.data
+
+import com.example.mysimplecleanarchitecture.domain.IMessageRepository
+import com.example.mysimplecleanarchitecture.domain.MessageEntity
+
+class MessageRepository(private val messageDataSource: IMessageDataSource): IMessageRepository {
+    override fun getWelcomeMessage(name: String) = messageDataSource.getMessageFromSource(name)
+}
